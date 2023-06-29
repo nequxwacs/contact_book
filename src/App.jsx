c7e391ee10249ed75133262c971e2be85d1e1865
Табылдиев Даниэль <dtabyldiev2006@gmail.com>
2023-06-29 15:51:34 +0600

    contact

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <main>
+      <header className='header'>
+      </header>
+
+      <section>
+
+      </section>
+    </main>
+  )
+}
+
+export default App
